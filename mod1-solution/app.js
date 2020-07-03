3487ac692c2570a6e8a45f0176c137062b3a3c37
David Reyes <rdavey61@gmail.com>
2020-07-03 17:16:47 -0400

    new folder

---
+++ mod1-solution/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+ 'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope','$filter'];
+
+  function LunchCheckController($scope, $filter) {
+    
+
+  }
