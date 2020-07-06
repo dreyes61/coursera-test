5f9e64f2284b872e20ea4877a5583e82bbee1aa6
David Reyes <rdavey61@gmail.com>
2020-07-05 23:17:04 -0400

    Changes

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -8,6 +8,32 @@
   LunchCheckController.$inject = ['$scope','$filter'];
 
   function LunchCheckController($scope, $filter) {
-    
+    $scope.foods ="";
+    $scope.response = "";
 
-  }
+    $scope.showMessage = function () {
+      if($scope.foods) {
+          $scope.color = "color:blue";
+          $scope.borderColor = "border-color:blue";
+          var splitter = ",";
+          var arrayOfFoods = $scope.foods.split(splitter);
+          var count = 0;
+          for (var i=0; i<arrayOfFoods.length; i++) {
+            if (!(arrayOfFoods[i].length == 0 || !arrayOfFoods[i].trim())) {
+              count += 1;
+            }
+      }
+      if(count>3){
+        $scope.response="Too much!";
+      }else{
+        $scope.response="Enjoy!";
+      }
+    }
+      else{
+        $scope.color = "color:red";
+        $scope.borderColor = "border-color:red";
+        $scope.response = "Please enter data first";
+      }
+    };
+};
+})();
